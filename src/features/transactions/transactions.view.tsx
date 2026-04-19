31aa1fb74dbd75eb0feb340f3927100e15bdc1cf
joseerisvaldo <joseerisvaldopg@gmail.com>
2026-04-19 17:48:03 -0300

    feat: update Transactions view layout and content for better user experience

--- src/features/transactions/transactions.view.tsx
+++ src/features/transactions/transactions.view.tsx
@@ -2,10 +2,20 @@ import TableTransactions from "./components/table-transactions";
 
 export default function Transactions() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">Transações</h1>
-      <p className="text-lg text-gray-600">Bem vindo!</p>
-
+    <div className="container mx-auto px-4 py-6">
+      <h1 className="text-2xl font-bold mb-4">Minhas Transações</h1>
+      <div>
+        <p className="text-gray-600 mb-2">
+          Aqui estão todas as suas transações financeiras recentes. Você pode
+          visualizar detalhes, editar ou excluir cada transação conforme
+          necessário.
+        </p>
+      </div>
+      <div className="mb-4">
+        <button className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 transition-colors cursor-pointer">
+          Adicionar Nova Transação
+        </button>
+      </div>
       <div className="mt-8">
         <TableTransactions />
       </div>
